d8e08197ca214f034e881bc5d6e257598f50fe61
Volker Richert <volker.richert@addmore.de>
2025-12-29 18:22:21 +0100

    quick handling remote_controll_ip without port Need to be invesigated

--- custom_components/e3dc_rscp/utils.py
+++ custom_components/e3dc_rscp/utils.py
@@ -20,36 +20,38 @@ async def initialize_farm_controller_flow_if_needed(hass, proxy: E3DCProxy, user
     remote_control_ip: str | None = proxy.get_remote_control_ip()
     _LOGGER.debug(f"Found remote control IP: {remote_control_ip}")
 
-    if (remote_control_ip):
+    if remote_control_ip:
         """ Initiate sub-flow for farm controller configuration. """
 
-        host, port = remote_control_ip.split(":")
-        port = int(port)
-        controller_found = False
+        parts = remote_control_ip.rsplit(":", 1)
+        if len(parts) == 2:
+            host = parts[0]
+            port = int(parts[1])
+            controller_found = False
 
-        """ try to find existing controller entry """
-        for entry in hass.config_entries.async_entries(DOMAIN):
-            is_host_and_port_match = (
-                entry.data.get(CONF_HOST) == host and entry.data.get(CONF_PORT) == port
-            )
-            is_title_match = entry.title == f'E3DC Farm Controller at {host}'
-            _LOGGER.debug(f"Checking existing entry {entry.title} for host/port match: {is_host_and_port_match}, title match: {is_title_match}")
-            if is_host_and_port_match or is_title_match:
-                controller_found = True
+            """ try to find existing controller entry """
+            for entry in hass.config_entries.async_entries(DOMAIN):
+                is_host_and_port_match = (
+                    entry.data.get(CONF_HOST) == host and entry.data.get(CONF_PORT) == port
+                )
+                is_title_match = entry.title == f'E3DC Farm Controller at {host}'
+                _LOGGER.debug(f"Checking existing entry {entry.title} for host/port match: {is_host_and_port_match}, title match: {is_title_match}")
+                if is_host_and_port_match or is_title_match:
+                    controller_found = True
 
-        if not controller_found:
-            _LOGGER.debug(f"Creating sub-flow for farm controller at {host}:{port}")
-            await hass.config_entries.flow.async_init(
-                DOMAIN,
-                context={
-                    "source": SOURCE_INTEGRATION_DISCOVERY,
-                    "title_placeholders": {"name": f'E3DC Farm Controller at {host}'}
-                },
-                data={
-                    CONF_HOST: host,
-                    CONF_PORT: port,
-                    CONF_USERNAME: username,
-                    CONF_PASSWORD: password,
-                    CONF_RSCPKEY: rscp,
-                },
-            )
+            if not controller_found:
+                _LOGGER.debug(f"Creating sub-flow for farm controller at {host}:{port}")
+                await hass.config_entries.flow.async_init(
+                    DOMAIN,
+                    context={
+                        "source": SOURCE_INTEGRATION_DISCOVERY,
+                        "title_placeholders": {"name": f'E3DC Farm Controller at {host}'}
+                    },
+                    data={
+                        CONF_HOST: host,
+                        CONF_PORT: port,
+                        CONF_USERNAME: username,
+                        CONF_PASSWORD: password,
+                        CONF_RSCPKEY: rscp,
+                    },
+                )
